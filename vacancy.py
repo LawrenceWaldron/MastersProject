e360c95cc54dc1b6b5ef67417378ecb5f2665e4d
LawrenceWaldron <lbw16@ic.ac.uk>
2020-01-17 17:47:49 +0000

    Vacancy commit 1

---
+++ vacancy.py
@@ -0,0 +1,33 @@
+from quippy.potential import Potential
+
+from ase.optimize import BFGS
+
+from ase.build import bulk
+
+from ase.io import write
+
+# The potential file should be in the same folder you are running the script in
+
+pot = Potential('IP GAP', param_filename='gp_new_hcp.xml')
+
+
+
+# Create a unit cell (from here it’s all ASE commands)
+
+n = 5
+
+unit_cell=bulk('Zr', crystalstructure='hcp', a=3.223352707047396, c=5.172324479026618) * (n, n, n)
+
+del unit_cell[n**3]
+
+unit_cell.set_calculator(pot)
+
+
+
+# Optimise the geometry of this cell – this won’t really do anything
+
+dyn = BFGS(unit_cell)
+
+dyn.run(fmax=0.001)
+
+write('output.xyz', unit_cell)
