148ce91dbc592a8ddc4fd04d68bcbe2b5c52cd19
LawrenceWaldron <lbw16@ic.ac.uk>
2020-01-22 16:50:40 +0000

    commit2 a/c ratio

--- eos.py
+++ eos.py
@@ -8,14 +8,13 @@ from ase.io import write
 pot = Potential('IP GAP', param_filename='gp_new_hcp.xml')
 
 a0=3.23
-c0=5.18
+
 
 traj = Trajectory('Zr.traj', 'w')
 
-eps = 0.1
-for a in a0 * np.linspace(1 - eps, 1 + eps, 50):
-    for c in c0 * np.linspace(1 - eps, 1 + eps, 50):
-        zr = bulk('Zr', 'hcp', a=a, c=c)
+eps = 0.02
+for a in a0 * np.linspace(1 - eps, 1 + eps, 100):
+        zr = bulk('Zr', 'hcp', a=a, c=a*((8/3)**(1/2)))
         zr.set_calculator(pot)
         zr.get_potential_energy()
         traj.write(zr)
